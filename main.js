a89cae486d8c51604de1aedbc3ba677291698320
khang-kunz <khangdev171998@gmail.com>
2023-02-24 00:15:36 +0700

    update today

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const left = `<img class="services-icon" src="./images/prev.png" alt="this-is-icon">`;
-const right = `<img class="services-icon" src="./images/next.png" alt="this-is-icon">`
+const right = `<img class="services-icon" src="./images/next.png" alt="this-is-icon">`;
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    autoplay: true,
-    smartSpeed: 500,
-    margin:30,
-    nav:true,
-    navText: [left, right],
-    dots: true,
-    mouseDrag: false,
-    autoplayHoverPause: true,
-    autoplayTimeout: 4500,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:4
-        }
-    }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  smartSpeed: 500,
+  margin: 30,
+  nav: true,
+  navText: [left, right],
+  dots: true,
+  mouseDrag: false,
+  autoplayHoverPause: true,
+  autoplayTimeout: 4500,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
 
 const upTop = document.getElementById("up-top");
 
@@ -45,21 +45,20 @@ function debounceFn(func, wait, immediate) {
 // Tối ưu hoá delay scroll giúp tài nguyên không quá tải
 
 window.addEventListener(
-    "scroll",
-    debounceFn(function () {
-      const scrollY = window.pageYOffset;
+  "scroll",
+  debounceFn(function () {
+    const scrollY = window.pageYOffset;
 
-      if (scrollY > 400) {
-        upTop.classList.add("active");
-      } else {
-        upTop.removeAttribute("class");
-      }
-      upTop.onclick = function () {
-        scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-      };
-    }, 100)
-  );
-  
+    if (scrollY > 400) {
+      upTop.classList.add("active");
+    } else {
+      upTop.removeAttribute("class");
+    }
+    upTop.onclick = function () {
+      scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    };
+  }, 100)
+);
